b6c153c4e2d609de6cf6952a0f2fc8cba964c928
Eviqt01 <izoooIz00013@gmail.com>
2025-11-22 18:10:11 +0800

    project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+	import Footer from './footer/footer.svelte';
+
+	let PlatformInformation = $state('');
+	let LanguageInformation = $state('');
+	let VersionInformation = $state('');
+	let NameInformation = $state('');
+	let ScreenResolutionInformation = $state('');
+	let ScreenWidthInformation = $state();
+	let ScreenHeightInformation = $state();
+	let NetworkStatusInformation = $state('');
+	let TimezoneInformation = $state('');
+	let LocalTimeInformation = $state('');
+
+	const GenerateInformation = () => {
+		PlatformInformation = navigator.platform;
+		LanguageInformation = navigator.language;
+		VersionInformation = navigator.appVersion;
+		NameInformation = navigator.appName;
+		ScreenResolutionInformation = `${window.screen.width} x ${window.screen.height}`;
+		ScreenWidthInformation = window.innerWidth;
+		ScreenHeightInformation = window.innerHeight;
+		NetworkStatusInformation = navigator.onLine ? 'Online' : 'Offline';
+
+		window.addEventListener('online', () => {
+			NetworkStatusInformation = 'Online';
+		});
+		window.addEventListener('offline', () => {
+			NetworkStatusInformation = 'Offline';
+		});
+
+		TimezoneInformation = Intl.DateTimeFormat().resolvedOptions().timeZone;
+		LocalTimeInformation = new Date().toLocaleString();
+	};
+
+	const ClearInformation = () => {
+		PlatformInformation = '';
+		LanguageInformation = '';
+		VersionInformation = '';
+		NameInformation = '';
+		ScreenResolutionInformation = '';
+		ScreenWidthInformation = '';
+		ScreenHeightInformation = '';
+		NetworkStatusInformation = '';
+		TimezoneInformation = '';
+		LocalTimeInformation = '';
+	};
+</script>
+
+<section class="bg-gray-100 min-h-screen flex flex-col gap-5 relative">
+	<h1 class="text-2xl font-bold text-center py-6 text-gray-200 bg-gray-700">
+		System Administration and Maintenance (Project)
+	</h1>
+	<div
+		class="border p-5 flex flex-col gap-5 rounded-md shadow-lg sm:max-w-200 sm:w-100 max-w-200 relative w-80 mr-auto ml-auto"
+	>
+		<h1 class="font-bold text-center font-sans text-2xl">System Information Report</h1>
+		<div class="flex flex-col gap-3">
+			<span class="flex gap-1"
+				>Platform: <span class="border-b border-black">{PlatformInformation}</span>
+			</span>
+			<span class="flex gap-1"
+				>Language: <span class="border-b border-black">{LanguageInformation}</span>
+			</span>
+			<span class="flex gap-1"
+				>Version: <span class="border-b border-black">{VersionInformation}</span>
+			</span>
+			<span class="flex gap-1"
+				>Name: <span class="border-b border-black">{NameInformation}</span>
+			</span>
+			<span
+				>Screen Resolution: <span class="border-b border-black">{ScreenResolutionInformation}</span>
+			</span>
+			<span
+				>Screen Width: <span class="border-b border-black">{ScreenWidthInformation}</span>
+			</span>
+			<span
+				>Screen Height: <span class="border-b border-black">{ScreenHeightInformation}</span>
+			</span>
+			<span
+				>Network Status: <span class="border-b border-black">{NetworkStatusInformation}</span></span
+			>
+			<span>Time Zone: <span class="border-b border-black">{TimezoneInformation}</span></span>
+			<span>Local Time: <span class="border-b border-black">{LocalTimeInformation}</span></span>
+		</div>
+
+		<div class="flex flex-col gap-2">
+			<button
+				class="bg-blue-700 active:scale-95 cursor-pointer text-white hover:bg-blue-600 rounded-sm py-1"
+				onclick={GenerateInformation}>Generate</button
+			>
+			<button
+				class="bg-gray-700 text-gray-200 active:scale-95 cursor-pointer hover:bg-gray-900 rounded-sm py-1"
+				onclick={ClearInformation}>Clear</button
+			>
+		</div>
+	</div>
+</section>
+<Footer></Footer>
---
+++ src/routes/footer/footer.svelte
@@ -0,0 +1,12 @@
+<footer
+	class="bg-gray-300 p-5 relative flex flex-col rounded-md flex-wrap items-center justify-center gap-3"
+>
+	<h1 class="font-bold font-mono text-3xl">PROJECT</h1>
+	<div class="flex gap-3">
+		<h1 class="bg-gray-700 text-gray-200 font-bold font-sans px-3 rounded-md">
+			Eviota, Jhun Jay Mars B.
+		</h1>
+		<h1 class="bg-gray-700 text-gray-200 font-bold font-sans px-3 rounded-md">BSIT 4i</h1>
+	</div>
+	<p>November 22, 2025</p>
+</footer>
